bc58660857fd79a8d0d6b4c1025dd3ebf16844ce
Nick <nicholaspirrello@gmail.com>
2017-12-22 17:10:21 -0500

    Second Commit
    Bridged the GUI and the .csv parser through structured nested dictionaries and lists.

--- prorate_data.py
+++ prorate_data.py
@@ -2,20 +2,20 @@
 #12/6/17
 #Prorate Data
 import csv
+from pprint import pprint
 
-def main():
-	init_parse('J:/AxeCreatives/Tasks/ProrateTask.csv')
+#def main():
+#init_parse('J:/AxeCreatives/Tasks/ProrateTask.csv')
 
 def init_parse(filename):
 	file = open(filename)
 	reader = csv.reader(file, delimiter=',')
 	data = list(reader)
-	gather_data(data)
+	return(gather_data(data))
 
 def gather_data(list):
 
-	#Need CustomerName, OfferName, ChargeStartDate, ChargeType, UnitPrice, Quantity, 
-	#TotalForCustomer, DomainName
+
 	customer_name_col = 0
 	offer_name_col = 0
 	charge_start_date_col = 0
@@ -23,7 +23,6 @@ def gather_data(list):
 	unit_price_col = 0
 	quantity_col = 0
 	total_for_customer_col = 0
-	domain_name_col = 0
 
 	for j in range(0,len(list[0])):
 		if list[0][j] == 'CustomerName':
@@ -40,25 +39,55 @@ def gather_data(list):
 			quantity_col = j
 		elif list[0][j] == 'TotalForCustomer':
 			total_for_customer_col = j
-		elif list[0][j] == 'DomainName':
-			domain_name_col = j
 
 	valid_data_list = []
 	for k in range(1,len(list)):
-		if list[k][charge_type_col] == 'CYCLE INSTANCE PRORATE' and  float(list[k][total_for_customer_col]) > 0:
+		if list[k][charge_type_col] == 'CYCLE INSTANCE PRORATE':
 			valid_data_list.append(list[k]) 
 
-	print(valid_data_list)
-	#parse_data(important_data_block,customer_name_col,offer_name_col,charge_start_date_col,charge_type_col,unit_price_col,quantity_col,total_for_customer_col,domain_name_col)
+	return(extract_data(valid_data_list, customer_name_col, offer_name_col, charge_type_col, charge_start_date_col, unit_price_col,quantity_col, total_for_customer_col))
 
-#def parse_data(list, cname_col, offername_col, chargedate_col, chargetype_col, unitprice_col, quantity_col, customertotal_col, dname_col):
+def extract_data(datalist, customer_name_col, offer_name_col, charge_type_col, charge_start_date_col, unit_price_col, quantity_col, total_for_customer_col):
+	extractlist = []
+	for row in range(0,len(datalist)):
+		cname = datalist[row][customer_name_col]
+		oname = datalist[row][offer_name_col]
+
+		ct = datalist[row][charge_type_col]
+
+		csd = datalist[row][charge_start_date_col]
+		up = datalist[row][unit_price_col]
+		qt = datalist[row][quantity_col]
+		tc = datalist[row][total_for_customer_col]
+
+		if is_in(extractlist,cname) == True:
+			for i in extractlist:
+				if i['customername'] == cname:
+					i['charges'][0].append(ct)
+					i['charges'][1].append(oname)
+					i['charges'][2].append(csd)
+					i['charges'][3].append(up)
+					i['charges'][4].append(qt)
+					i['charges'][5].append(tc)
+					i['total'] = str(round(float(i['total']) + round(float(tc),2),2))
+		else:
+			new_data = {'customername':cname,'charges':[[ct],[oname],[csd],[up],[qt],[tc]],'total':str(round(float(tc),2))}
+			extractlist.append(new_data)
+
+	longest_row_count = len(extractlist[0]['charges'][1])
+	for j in extractlist:
+		nextinstance = len(j['charges'][1])
+		if nextinstance > longest_row_count:
+			longest_row_count = nextinstance
+
+	extractlist.append(longest_row_count)
+	pprint(extractlist)
+	return(extractlist)
 
 
-
-
-
-
-
-
-if __name__ == '__main__':
-	main()
+def is_in(list, name):
+	#If the specified name is found in the list, return True, otherwise False
+	for i in list:
+		if i['customername'] == name:
+			return(True)
+	return(False)
--- testgui.py
+++ testgui.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QPushButton, QTableWidget,QTableWidgetItem,QGridLayout,QHeaderView, QFileDialog, QInputDialog, QLineEdit, QMessageBox
 import sys
+import prorate_data
 
 class Summary(QWidget):
 	def __init__(self):
@@ -10,49 +11,70 @@ class Summary(QWidget):
 	def initUI(self):
 
 
+		#Swap OfferName to the end, set a defaultSectionSize for the 4 other columns and strecth the offer name
+		#For the nestedTable
 
-		data = [['JJS Transportation',['Plan 1','Plan 2'],['11.09.17','12.03.17'],['Monthly','Yearly'],['$4.95','$5.95'],'$109.67'],['KIDSMILES',['Plan 2'],['11.09.17'],['Monthly'],['$4.95'],'$16.85'],['HELFNER',['Plan 3'],['11.09.17'],['Monthly'],['$4.95'],'$56.34'],5]
+
+		import_bttn = QPushButton('Import')
+		reset_bttn = QPushButton('Reset')
+		enlarged_text = QLineEdit()
+		enlarged_text.setReadOnly(True)
+
+
+
+		data = prorate_data.init_parse('J://AxeCreatives/Tasks/ProrateTask.csv')
+	
 		self.tableWidget = QTableWidget()
 		self.tableWidget.setColumnCount(3)
 		self.tableWidget.setRowCount(len(data)-1)
 		self.tableWidget.setHorizontalHeaderLabels("Company Name;Total;Company Charges".split(";"))
 
-		self.tableWidget.horizontalHeader().setDefaultSectionSize(160)
+		self.tableWidget.horizontalHeader().setDefaultSectionSize(120)
 		self.tableWidget.horizontalHeader().setStretchLastSection(True)
 
 		self.tableWidget.verticalHeader().setDefaultSectionSize(56+(30 * (data[len(data)-1]-1)))
-		#for row in range(0,len(data)):
+		self.tableWidget.verticalScrollBar().setStyleSheet("QScrollBar {width:0px;}")
 		
-		
-		for table in range(0,3):
+
+		for table in range(0,len(data)-1):
 			something = self.create_nested_table(data[len(data)-1])
-			for on in range(0,len(data[table][1])):
-				something.setItem(on,0,QTableWidgetItem(data[table][1][on]))
-			for sd in range(0,len(data[table][2])):
-				something.setItem(sd,1,QTableWidgetItem(data[table][2][sd]))
-			for ct in range(0,len(data[table][3])):
-				something.setItem(ct,2,QTableWidgetItem(data[table][3][ct]))
-			for up in range(0,len(data[table][4])):
-				something.setItem(up,3,QTableWidgetItem(data[table][4][up]))
+			for offer in range(0, len(data[table]['charges'][1])):
+				something.setItem(offer,0,QTableWidgetItem(data[table]['charges'][1][offer]))
+			for sd in range(0,len(data[table]['charges'][2])):
+				something.setItem(sd,1,QTableWidgetItem(data[table]['charges'][2][sd]))
+			for up in range(0,len(data[table]['charges'][3])):
+				something.setItem(up,2,QTableWidgetItem(data[table]['charges'][3][up]))
+			for qt in range(0,len(data[table]['charges'][4])):
+				something.setItem(qt,3,QTableWidgetItem(data[table]['charges'][4][qt]))
+			for tc in range(0,len(data[table]['charges'][5])):
+				something.setItem(tc,4,QTableWidgetItem(data[table]['charges'][5][tc]))
 			self.tableWidget.setCellWidget(table,2,something)
-			self.tableWidget.setItem(table, 0, QTableWidgetItem(data[table][0]))
-			self.tableWidget.setItem(table, 1, QTableWidgetItem(data[table][5]))
+			self.tableWidget.setItem(table, 0, QTableWidgetItem(data[table]['customername']))
+			self.tableWidget.setItem(table, 1, QTableWidgetItem(data[table]['total']))
 
 		grid = QGridLayout()
 		grid.addWidget(self.tableWidget,1,1,5,5)
+		grid.addWidget(enlarged_text,6,1,1,2)
+		grid.addWidget(reset_bttn,6,4,1,1)
+		grid.addWidget(import_bttn,6,5,1,1)
 
 		self.setLayout(grid)
-		self.setGeometry(300,300,1000,800)
+		self.setGeometry(200,200,1000,800)
 		self.show()
 
 	def create_nested_table(self, rowcount):
 		nestedTableWidget = QTableWidget()
-		nestedTableWidget.setColumnCount(4)
+		nestedTableWidget.setColumnCount(5)
 		nestedTableWidget.setRowCount(rowcount)
-		nestedTableWidget.setHorizontalHeaderLabels("Offer Name;Start Date;Charge Type;Unit Price".split(";"))
+		nestedTableWidget.setHorizontalHeaderLabels("Offer Name;Start Date;Unit Price;Quantity;Total Price".split(";"))
 		nestedTableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 		nestedTableWidget.verticalHeader().setDefaultSectionSize(30)
+		#nestedTableWidget.cellClicked.connect(self.cell_selected,nestedTableWidget)
 		return nestedTableWidget
+
+	def cell_selected(self, row, column, nested):
+		item = nested.itemAt(row, column)
+		enlarged_text.setText(item.text())
 
 
 
